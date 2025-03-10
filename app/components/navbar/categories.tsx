425a8f2d354024f94aefc89480f9c13033ec2a22
Steven Audrey Daye <dev.stevendaye@yahoo.com>
2025-03-10 01:21:49 +0100

    Added Property Categories & Routes

---
+++ app/components/navbar/categories.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+
+import { Container } from "../commons/container";
+import { CategoryBox } from "./category-box";
+import { categories } from "@/app/utils";
+
+export const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+
+  const isHomePage = pathname === "/";
+
+  if (!isHomePage) return null;
+
+  return (
+    <Container>
+      <div className="flex items-center justify-between pt-4 overflow-x-auto">
+        {categories.map((item) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            selected={category === item.label}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Container } from "../commons/container";
+import { Categories } from "./categories";
 import { Logo } from "./logo";
 import { Search } from "./search";
 import { UserMenu } from "./user-menu";
@@ -22,6 +23,8 @@ export const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+
+      <Categories />
     </div>
   );
 };
